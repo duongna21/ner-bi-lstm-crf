571f0a5b21dbd2190a1a5cf6e9e9a2548f4a18fc
thanhlt998 <thanhlt998@gmail.com>
2019-11-26 00:26:28 +0700

    Fix vocab

--- vocab.py
+++ vocab.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 from fasttext import FastText
 from gensim.models.keyedvectors import KeyedVectors
 import math
+import string
+
+NUM = '<NUM>'
+PUNCTUATION = '<PUNCTUATION>'
+UNK = '<UNK>'
 
 
 class Vocab:
@@ -26,12 +31,12 @@ class Vocab:
         self.model = FastText.load_model(path)
 
         self.embedding_dim = self.model.get_dimension()
-        self.words = self.model.words
+        self.words = [*self.model.words, NUM, PUNCTUATION]
         self.word2index = {word: i for i, word in enumerate(self.words)}
         self.unk_index = len(self.words)
         self.padding_index = len(self.words) + 1
         self.embeddings = nn.Embedding.from_pretrained(torch.cat([torch.tensor(self.model.get_input_matrix()),
-                                                                  torch.rand(1, self.embedding_dim,
+                                                                  torch.rand(3, self.embedding_dim,
                                                                              dtype=torch.float).uniform_(
                                                                       - math.sqrt(3 / self.embedding_dim),
                                                                       math.sqrt(3 / self.embedding_dim)),
@@ -43,12 +48,12 @@ class Vocab:
     def load_pretrained_embedding_from_gensim_fasttext_model(self, path, freeze):
         self.model = KeyedVectors.load_word2vec_format(path, binary=True)
         self.embedding_dim = self.model.vector_size
-        self.words = self.model.index2word
+        self.words = [*self.model.index2word, NUM, PUNCTUATION]
         self.word2index = {word: i for i, word in enumerate(self.words)}
         self.unk_index = len(self.words)
         self.padding_index = len(self.words) + 1
         self.embeddings = nn.Embedding.from_pretrained(torch.cat([torch.tensor(self.model.vectors),
-                                                                  torch.rand(1, self.embedding_dim,
+                                                                  torch.rand(3, self.embedding_dim,
                                                                              dtype=torch.float).uniform_(
                                                                       - math.sqrt(3 / self.embedding_dim),
                                                                       math.sqrt(3 / self.embedding_dim)),
@@ -60,5 +65,16 @@ class Vocab:
     def get_embedding(self):
         return self.embeddings
 
-    def word_to_index(self, word):
+    @staticmethod
+    def process_word(word, lowercase):
+        if lowercase:
+            word = word.lower()
+        if word[0].isdigit():
+            word = NUM
+        elif word in string.punctuation:
+            word = PUNCTUATION
+        return word
+
+    def word_to_index(self, word: str, lowercase=True):
+        word = self.process_word(word, lowercase)
         return self.word2index.get(word, self.unk_index)
