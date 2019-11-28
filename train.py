1e2445e7643abdc338946276580cbc78b5d7c3f6
Vu Tien Dat <vudat1710@gmail.com>
2019-11-28 08:35:42 +0700

    add argument parser

--- train.py
+++ train.py
@@ -10,12 +10,35 @@ from bilstm_crf import BiLSTMCrf
 import dataset
 import evaluator
 from sentence import Sentence
+import argparse
 
 
 def adjust_learning_rate(optimizer, learning_rate, epoch, learning_rate_decay):
     lr = learning_rate / (1 + learning_rate_decay * (epoch - 1))
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group['lr'] = args.lr
+
+def parse():
+    parser = argparse.ArgumentParser(description='BiLSTM-CRF model')
+    # parser.add_argument('-m', '--mode', help='Training mode or testing mode: value either train or test', required=True)
+    parser.add_argument('-tr', '--trainpath', help='Training file location', default='data/data/train.txt')
+    parser.add_argument('-te', '--testpath', help='Test file location', default='data/data/test.txt')
+    parser.add_argument('-de', '--devpath', help='Development file location', default='data/data/dev.txt')
+    parser.add_argument('-pr', '--pretrained_path', default='data/pretrained_embedding/fasttext_pretrained_embeddings_300.bin', help='Pretrained word embedding used in training')
+    parser.add_argument('-wb', '--word_embedding_dim', type=int, default=300, help='Number of word embedding dimensions')
+    parser.add_argument('-cb', '--char_embedding_dim', type=int, default=100, help='Number of character embedding dimensions')
+    parser.add_argument('-b', '--batch_size', type=int, default=8, help='Batch size')
+    parser.add_argument('-wi', '--context_hidden_dim', type=int, default=150 * 2, help='Context hidden size')
+    parser.add_argument('-ci', '--character_hidden_dim', type=int, default=100 * 2, help='Character hidden size')
+    # parser.add_argument('-ls', '--patience', type=int, default=10, help='Patience used in early stopping')
+    parser.add_argument('-lr', '--lr', type=float, default=0.0035, help='Learning rate')
+    parser.add_argument('-d', '--dropout', type=float, default=0.35, help='Dropout probability for lstm and dropout layers')
+    parser.add_argument('-cp', '--checkpoint_dir', default='data/model', help='Checkpoint directory')
+    parser.add_argument('-ep', '--epochs', type=int, default=25, help='Number of epochs')
+    parser.add_argument('-dc', '--learning_rate_decay', default=0.06, type=float, help='Decay rate in each epoch')
+
+    args = parser.parse_args()
+    return args
 
 
 def train(
@@ -34,7 +57,7 @@ def train(
         save_dir
 ):
     for epoch in range(start_epoch, epochs + 1):
-        adjust_learning_rate(optimizer, learning_rate, epoch, learning_rate_decay)
+        adjust_learning_rate(optimizer, args.lr, epoch, args.learning_rate_decay)
         model.train()
         current_loss = 0
         for ((batch_sentence_word_indexes,
@@ -70,7 +93,7 @@ def train(
 
         if test_f1_score > best_test_score:
             best_test_score = test_f1_score
-            directory = os.path.join(save_dir, f'{character_hidden_dim}_{context_hidden_dim}_{crf_loss_reduction}')
+            directory = os.path.join(save_dir, f'{args.character_hidden_dim}_{args.context_hidden_dim}_{crf_loss_reduction}')
             torch.save({
                 'epoch': epoch,
                 'model': model.state_dict(),
@@ -81,7 +104,7 @@ def train(
             }, os.path.join(directory, "best_model.tar"))
 
         if epoch % save_every == 0:
-            directory = os.path.join(save_dir, f'{character_hidden_dim}_{context_hidden_dim}_{crf_loss_reduction}')
+            directory = os.path.join(save_dir, f'{args.character_hidden_dim}_{args.context_hidden_dim}_{crf_loss_reduction}')
             if not os.path.exists(directory):
                 os.makedirs(directory)
 
@@ -119,7 +142,7 @@ def load_model(model_fn, voc, character_embedding_dim, character_hidden_dim, con
                       dropout=dropout,
                       crf_loss_reduction=crf_loss_reduction)
 
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     if model_fn is not None:
         checkpoint = torch.load(model_fn)
@@ -144,14 +167,14 @@ def load_model(model_fn, voc, character_embedding_dim, character_hidden_dim, con
 
 
 if __name__ == '__main__':
+    args = parse()
     print('Loading vocab...')
-    voc = vocab.Vocab('data/pretrained_embedding/fasttext_pretrained_embeddings_300.bin', freeze=True)
-    batch_size = 8
+    voc = vocab.Vocab(args.pretrained_path, freeze=True)
 
     print('Loading data ...')
-    train_sentences = [Sentence(sentence, voc) for sentence in utils.read_data('data/data/train.txt')]
-    dev_sentences = [Sentence(sentence, voc) for sentence in utils.read_data('data/data/dev.txt')]
-    test_sentences = [Sentence(sentence, voc) for sentence in utils.read_data('data/data/test.txt')]
+    train_sentences = [Sentence(sentence, voc) for sentence in utils.read_data(args.trainpath)]
+    dev_sentences = [Sentence(sentence, voc) for sentence in utils.read_data(args.devpath)]
+    test_sentences = [Sentence(sentence, voc) for sentence in utils.read_data(args.testpath)]
 
     train_ds = dataset.Dataset(train_sentences, word_padding_idx=voc.padding_index,
                                pos_padding_idx=const.POS_PADDING_IDX,
@@ -169,19 +192,12 @@ if __name__ == '__main__':
                               character_padding_idx=const.CHARACTER2INDEX['<PAD>'],
                               tag_padding_idx=const.CHUNK_PADDING_IDX)
 
-    train_dl = dataset.DataLoader(train_ds, batch_size=batch_size)
-    dev_dl = dataset.DataLoader(dev_ds, batch_size=batch_size)
-    test_dl = dataset.DataLoader(test_ds, batch_size=batch_size)
+    train_dl = dataset.DataLoader(train_ds, batch_size=args.batch_size)
+    dev_dl = dataset.DataLoader(dev_ds, batch_size=args.batch_size)
+    test_dl = dataset.DataLoader(test_ds, batch_size=args.batch_size)
 
-    word_embedding_dim = 300
-    character_embedding_dim = 100
-    character_hidden_dim = 100 * 2
-    context_hidden_dim = 150 * 2
-    learning_rate = 0.0035
-    learning_rate_decay = 0.06
-    momentum = 0.9
-    dropout = 0.35
-    epochs = 30
+    # word_embedding_dim = 300
+    # momentum = 0.9
     crf_loss_reduction = 'token_mean'
 
     # model_fn = 'data/model/200_300_token_mean/3_checkpoint.tar'
@@ -189,10 +205,10 @@ if __name__ == '__main__':
 
     model, optimizer, epoch, all_losses, eval_losses, test_scores, best_test_score = load_model(model_fn,
                                                                                                 voc,
-                                                                                                character_embedding_dim,
-                                                                                                character_hidden_dim,
-                                                                                                context_hidden_dim,
-                                                                                                dropout,
+                                                                                                args.character_embedding_dim,
+                                                                                                args.character_hidden_dim,
+                                                                                                args.context_hidden_dim,
+                                                                                                args.dropout,
                                                                                                 crf_loss_reduction)
 
     print('Training...')
@@ -202,12 +218,12 @@ if __name__ == '__main__':
         train_dl,
         dev_dl,
         test_dl,
-        epochs,
+        args.epochs,
         epoch,
         all_losses,
         eval_losses,
         test_scores,
         best_test_score,
         save_every=5,
-        save_dir='data/model'
+        save_dir=args.checkpoint_dir
     )
